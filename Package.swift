04490d9977ea59c22a1fe63603c5e2b0d480a4bb
galy <galy@ctera.com>
2020-12-03 17:06:12 +0200

    CHANGE  - bug fix, using supported functions for iOS 13

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
 	name: "CteraSwiftLib",
 	defaultLocalization: "en",
-	platforms: [.iOS(.v13), .macOS(.v11)],
+	platforms: [.iOS(.v13), .macOS(.v10_15)],
 	products: [
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
