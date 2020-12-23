cf77f3d9d7c78ec817f56d92b6b0d6b004f27bfd
galy <galy@ctera.com>
2020-12-23 11:48:28 +0200

    CHANGE  - increased minimum iOS version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
 	name: "CteraSwiftLib",
 	defaultLocalization: "en",
-	platforms: [.iOS(.v13), .macOS(.v10_15)],
+	platforms: [.iOS("13.2"), .macOS(.v10_15)],
 	products: [
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
