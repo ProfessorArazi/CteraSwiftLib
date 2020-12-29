6d4fa3fd08c2ed1ccbf9074ac44dc36223de14f6
galy <galy@ctera.com>
2020-12-29 11:17:11 +0200

    DELETE  - redundant requirements for FileCache

--- Sources/CteraCache/FileCache.swift
+++ Sources/CteraCache/FileCache.swift
@@ -11,24 +11,11 @@ import CteraUtil
 import CteraModels
 
 public protocol FileCache {
-	init()
-	
-	var cache: [String: CacheItem] { get }
-	
-	var cacheSize: Int64 { get }
-	
 	func item(for item: ItemInfoDto) -> CacheItem?
 	
 	func save(file: URL, with item: ItemInfoDto)
 	
 	func provide(item: CacheItem, handler: @escaping (Swift.Result<URL, Error>)->()) -> ProgressTask
-}
-
-public extension FileCache {
-	var cacheSize: Int64 {
-		cache.compactMap { $0.value.item.size }
-			.reduce(0, +)
-	}
 }
 
 public struct CacheItem: Codable {
