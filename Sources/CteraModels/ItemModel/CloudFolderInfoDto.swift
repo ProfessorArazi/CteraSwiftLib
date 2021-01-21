834b352155067a04cc705fe08382c9b610cff66e
galy <galy@ctera.com>
2021-01-21 22:29:42 +0200

    XP-75
    CHANGE
     - made values optional

--- Sources/CteraModels/ItemModel/CloudFolderInfoDto.swift
+++ Sources/CteraModels/ItemModel/CloudFolderInfoDto.swift
@@ -8,10 +8,10 @@
 public struct CloudFolderInfoDto: Codable, Equatable, Hashable {
 	public var uid: Int = 0
 	public var groupUid: Int = 0
-	public var ownerUid: Int = 0
+	public var ownerUid: Int?
 	public var name: String = ""
 	public var type: String = ""
-	public var ownerFriendlyName: String = ""
+	public var ownerFriendlyName: String?
 	public var isShared: Bool = false
 	public var passphraseProtected: Bool = false
 	
